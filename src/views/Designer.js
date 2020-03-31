b4ff4ce52be4e99d3490d9be6a0f570060e0bff3
MattNewhall <matthew.newhall@durham.ac.uk>
2020-03-31 21:48:15 +0100

    Basic Designer complete, all bays posting!

--- src/views/Designer.js
+++ src/views/Designer.js
@@ -3,6 +3,7 @@ import {Accordion, AccordionPanel, Box, Grommet, Meter, Select, Stack, Text} fro
 import {Alert, Col, Container, Jumbotron, Row} from "react-bootstrap";
 import {SettingsOption, Add, Trash, DocumentExcel} from "grommet-icons";
 import {grommet} from "grommet/themes";
+import Loading from "../components/Loading";
 
 function getBaysInZone(query) {
 	return new Promise((resolve, reject) => {
@@ -23,7 +24,7 @@ function getBaysInZone(query) {
 
 
 class Designer extends Component {
-	componentWillMount() {
+	componentDidMount() {
 
 		let zonesList;
 		fetch("http://127.0.0.1:3001/stockTake/getZones", {
@@ -35,191 +36,211 @@ class Designer extends Component {
 		})
 		 .then(res => res.json())
 		 .then((data) => {
-			 zonesList = data;
+			 zonesList = data.zones;
 
 			 let bayQuerys = [];
 			 for (let currentZone = 0; currentZone < zonesList.length; currentZone++) {
-			 	bayQuerys.push({"zone":data.zones[currentZone].zone});
+			 	bayQuerys.push({"zone":zonesList[currentZone].zone});
 			 }
 
 			 let bayList = [];
 
 			 bayQuerys.forEach(
 			 	(query)=>{
-			 		bayList.push(getBaysInZone(query))
+			 		bayList.push(getBaysInZone(query));
 			 	});
 			 Promise.all(bayList).then((allBayData)=>{
 			 	let x = {};
 			 	allBayData.forEach(
 			 		(aLoop) => {
 			 			let temp = aLoop.bays;
-			 			let temp1 = {};
+			 			let temp1 = [];
 			 			temp.forEach(
 			 				(loop) => {
-			 					temp1[loop.bay] = {xVal:loop.xVal , yVal:loop.yVal, xSize:loop.xSize, ySize:loop.ySize, name:loop.bay}
+			 					temp1.push({name:loop.bay, xVal:loop.xVal , yVal:loop.yVal, xSize:loop.xSize, ySize:loop.ySize});
 							});
-			 			x[aLoop.bays[0].zone] =temp1
+			 			x[aLoop.bays[0].zone] = temp1;
 			 		});
-			 	this.setState({bays: x, zones: zonesList.zones});
-			 	console.log("Complete");
-			 	render();
+
+			 	this.loading = false;
+			 	console.log(x);
+			 	console.log(zonesList);
+			 	this.setState({bays: x, zones: zonesList});
 			 });
 		 })
 	}
 
 	constructor(props){
 		super(props);
-
+		this.loading = true;
+		this.state = {bays: {}, zones: []};
 	}
 
 	render() {
-		return (
-			<Container>
-				<Row>
-					<Col md={9}>
-			<div>
-				<br/>
-				<br/>
-				<Jumbotron >
-					<Container>
-						<h1>Warehouse Designer</h1>
+		if (this.loading){
+			return <Loading/>
+		}else {
+			return (
+				<Container>
+					<Row>
+						<Col md={9}>
+							<div>
+								<br/>
+								<br/>
+								<Jumbotron>
+									<Container>
+										<h1>Warehouse Designer</h1>
 
-							<li>We will have a list of zones, that will collapse into an accordion component (search grommet accordion to see what i mean)</li>
-							<li>further accordions will expand to show the bays, each bay will have a gear icon</li>
-							<li>the gear icon will yield a pop out (not sure which react framework will support this.)</li>
-							<li>the pop out will allow user to increase the dimensions of the bay or decrease them if no bays will be lost.</li>
-							<li>the popout we see here will be the same component visible in the gear icon in the stocktake component.</li>
+										<li>We will have a list of zones, that will collapse into an accordion component
+											(search grommet accordion to see what i mean)
+										</li>
+										<li>further accordions will expand to show the bays, each bay will have a gear
+											icon
+										</li>
+										<li>the gear icon will yield a pop out (not sure which react framework will
+											support this.)
+										</li>
+										<li>the pop out will allow user to increase the dimensions of the bay or
+											decrease them if no bays will be lost.
+										</li>
+										<li>the popout we see here will be the same component visible in the gear icon
+											in the stocktake component.
+										</li>
 
-					</Container>
-				</Jumbotron>
+									</Container>
+								</Jumbotron>
+
+								<Accordion>
+
+									{this.state.zones.map(i => {
+										return <AccordionPanel label={i.zone}>
+											<Box pad="medium" background="light-2">
+												<Alert variant={'info'} style={{alignContent: 'left'}}>
+													<Row>
+														<Col>
+															<SettingsOption color="#000066"/>
+														</Col>
+
+														Edit Zone Name
+
+													</Row>
+												</Alert>
+												<Alert variant={'danger'} style={{alignContent: 'left'}}>
+													<Row>
+														<Col>
+															<Trash color="#000066"/>
+														</Col>
+
+														Delete this Zone
+
+													</Row>
+												</Alert>
+												<Alert variant={'success'} style={{alignContent: 'left'}}>
+													<Row>
+														<Col>
+															<Add color="#000066"/>
+														</Col>
+
+														Add Bay
+
+													</Row>
+												</Alert>
+
+												{this.state.bays[i.zone].map(z => {
+
+													return <Accordion>
+														<AccordionPanel label={'Bay: ' + z.name}>
+															<Box pad="medium" background="light-2">
+																<text>{z.ySize} trays high and {z.xSize} trays
+																	wide:
+																</text>
+																<Alert variant={'success'}
+																	   style={{alignContent: 'left'}}>
+																	<Row>
+																		<Col>
+																			<SettingsOption color="#000066"/>
+																		</Col>
+
+																		Change The dimensions of this bay
+
+																	</Row>
+																</Alert>
+																<Alert variant={'danger'}
+																	   style={{alignContent: 'left'}}>
+																	<Row>
+																		<Col>
+																			<Trash color="#000066"/>
+																		</Col>
+
+																		Delete This Bay
+
+																	</Row>
+																</Alert>
+															</Box>
+														</AccordionPanel>
+													</Accordion>
+
+												})}
 
 
-				<Accordion>
+											</Box>
+										</AccordionPanel>
+									})}
+								</Accordion>
 
+							</div>
+						</Col>
+						<Col md={3}>
+							<li style={{color: '#ffffff'}}>Hello World</li>
 
-				{this.state.zones.map(i =>{
-					return <AccordionPanel label={i.zone}>
-						<Box pad="medium" background="light-2">
-							<Alert  variant={'info'} style={{alignContent:'left'}}>
+							<li style={{color: '#ffffff'}}>Hello World</li>
+							<Alert variant={'success'} style={{alignContent: 'left'}}>
 								<Row>
 									<Col>
-										<SettingsOption color = "#000066"/>
+										<Add color="#000066"/>
 									</Col>
 
-									Edit Zone Name
+									Add a new zone
 
 								</Row>
 							</Alert>
-							<Alert  variant={'danger'} style={{alignContent:'left'}}>
+							<Alert variant={'danger'} style={{alignContent: 'left'}}>
 								<Row>
 									<Col>
-										<Trash color = "#000066"/>
+										<Add color="#000066"/>
 									</Col>
 
-									Delete this Zone
+									Download Previous stock takes from
 
 								</Row>
 							</Alert>
-							<Alert  variant={'success'} style={{alignContent:'left'}}>
-								<Row>
-									<Col>
-										<Add color = "#000066"/>
-									</Col>
-
-									Add Bay
-
-								</Row>
-							</Alert>
-							{this.state.bays[i.zone].map(z=>{
-								return z.map(a=>{
-
-									return <Accordion>
-											<AccordionPanel label={'Bay: '+a.name}>
-										<Box pad="medium" background="light-2">
-											<text>{a.ySize}  trays high and {a.xSize} trays wide:</text>
-											<Alert  variant={'success'} style={{alignContent:'left'}}>
-												<Row>
-													<Col>
-														<SettingsOption color = "#000066"/>
-													</Col>
-
-													Change The dimensions of this bay
-
-												</Row>
-											</Alert>
-											<Alert  variant={'danger'} style={{alignContent:'left'}}>
-												<Row>
-													<Col>
-														<Trash color = "#000066"/>
-													</Col>
-
-													Delete This Bay
-
-												</Row>
-											</Alert>
+							<Grommet theme={grommet}>
+								<Box align="center" pad="large">
+									<Stack anchor="center">
+										<Meter
+											type="circle"
+											background="light-4"
+											values={[{value: 30}]}
+											size="small"
+											thickness="medium"
+										/>
+										<Box direction="row" align="center" pad={{bottom: "medium"}}>
+											<Text size="xlarge" weight="bold">
+												{30}
+											</Text>
+											<Text size="small">% </Text>
 										</Box>
-									</AccordionPanel>
-									</Accordion>
-
-									})
-							})}
-
-
-						</Box>
-					</AccordionPanel>
-				})}
-				</Accordion>
-
-			</div>
-					</Col>
-					<Col md={3}>
-						<li style={{color:'#ffffff'}}>Hello World </li>
-
-						<li style={{color:'#ffffff'}}>Hello World </li>
-						<Alert  variant={'success'} style={{alignContent:'left'}}>
-							<Row>
-								<Col>
-									<Add color = "#000066"/>
-								</Col>
-
-								Add a new zone
-
-							</Row>
-						</Alert>
-						<Alert  variant={'danger'} style={{alignContent:'left'}}>
-							<Row>
-								<Col>
-									<Add color = "#000066"/>
-								</Col>
-
-								Download Previous stock takes from
-
-							</Row>
-						</Alert>
-						<Grommet theme={grommet}>
-							<Box align="center" pad="large">
-								<Stack anchor="center">
-									<Meter
-										type="circle"
-										background="light-4"
-										values={[{ value: 30 }]}
-										size="small"
-										thickness="medium"
-									/>
-									<Box direction="row" align="center" pad={{ bottom: "medium" }}>
-										<Text size="xlarge" weight="bold">
-											{30}
-										</Text>
-										<Text size="small">% </Text>
-									</Box>
-								</Stack>  <Text  weight="bold" size="small" style={{textAlign:'-webkit-center'}}> Vacant, 338 free of 1125 spaces left. </Text>
-							</Box></Grommet>
+									</Stack> <Text weight="bold" size="small"
+												   style={{textAlign: '-webkit-center'}}> Vacant, 338 free of 1125
+									spaces left. </Text>
+								</Box></Grommet>
 
 
-					</Col>
-				</Row>
-			</Container>
-		);
+						</Col>
+					</Row>
+				</Container>
+			);
+		}
 	}
 }
 
