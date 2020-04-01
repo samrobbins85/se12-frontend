8f75f771cec49bcf5b7bad69a5c9324693c21e14
Karina <karina.talibzhanova@durham.ac.uk>
2020-04-01 16:02:16 +0100

    home page no longer looks like a placeholder

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,10 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src="https://durham.foodbank.org.uk/wp-content/uploads/sites/153/2016/05/Durham-Three-Colour-logo.png" alt="Durham Foodbank" width="120" />
-    <h1 className="mb-4">Durham Foodbank Stocktake System</h1>
+    <h1 className="mb-4">Stock Management System</h1>
 
     <p className="lead">
-      We can put some nice words here
+      A stock management system produced for Durham Foodbank
     </p>
   </div>
 );
